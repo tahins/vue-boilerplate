9843b33f55ea1bbf1bb4e9e42bcc0b6b5a3b96ce
yeaseenur.rahman <yeaseenur.rahman@infolytx.com>
2020-05-10 00:36:22 +0600

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from "./pages/Home";
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        }
+    ],
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { x: 0, y: 0 };
+        }
+    }
+});
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    pwa: {
+        name: '@tahins',
+        themeColor: '#000000'
+    }
+}
